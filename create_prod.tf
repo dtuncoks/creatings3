07a02cb3655b730b63fba155b5f890a65291573c
dtuncoks <dtuncoks@gmail.com>
2021-12-30 14:28:03 -0500

    Defined initial web security Group

---
+++ create_prod.tf
@@ -0,0 +1,41 @@
+provider "aws"{
+  alias   = "amazon"
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "prod_example" {
+
+  bucket = "prod-30122021"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound amd everything outbound"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
